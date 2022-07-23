fcf25f0280dc8118a847d4b8cd5a0f8812c48442
beyond_th0ught <yadavkhushboo7653@gmail.com>
2022-07-24 02:23:13 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Main from './flexy/main'
-// import Json from './json/json'
+// import Main from './flexy/main'
+import Json from './json/json'
 
 
 const App = () => {
   return (
     <div>
-        <Main/>
+        <Json/>
 
     </div>
   )

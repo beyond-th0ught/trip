a3934b23a82722b7fbab1d2fda16d0d57eb20dd3
beyond_th0ught <yadavkhushboo7653@gmail.com>
2022-07-23 20:18:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Main from './flexy/main'
+import Json from './json/json'
+
+
+const App = () => {
+  return (
+    <div>
+        <Json/>
+
+    </div>
+  )
+}
+export default App

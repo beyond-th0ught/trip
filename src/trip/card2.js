f77dd076b2de5d8e9db527e4598bc5b6c43c45fe
beyond_th0ught <yadavkhushboo7653@gmail.com>
2022-08-09 18:13:09 +0530

    trip new

--- src/trip/card2.js
+++ src/trip/card2.js
@@ -5,7 +5,7 @@ import Card2page2 from './card2page2'
 export default function card2() {
   return (
     <div class="flex mx-10 pt-8">
-        <div class="">
+        <div class=" ">
             <Card2page1/>
         </div>
         <div class="">
--- src/trip/card2page2.js
+++ src/trip/card2page2.js
@@ -3,8 +3,8 @@ import Circle1 from './circle'
 
 export default function card2page2() {
   return (
-    <div class="flex flex-col justify-center items-center ml-4 pt-10">
-    <div class="bg-black text-white p-5 px-16 rounded-lg ">
+    <div class="flex flex-col justify-center items-center ml-5  pt-10">
+    <div class="bg-black text-white p-5 px-11 rounded-lg ">
     <h1>Photo/Video</h1>
         <ul>
             <li class="hover:text-red-500 hover:cursor-pointer hover:fill-red-500">Overview</li>          
@@ -15,7 +15,7 @@ export default function card2page2() {
         </ul>
     </div>
     <div class="mt-3">
-    <button class="bg-red-500 text-center text-sm text-white py-1 px-16 rounded-md">
+    <button class="bg-red-500 text-center text-sm text-white py-1 px-11 rounded-md">
             Book Now <br/>
             <label class="text-white text-lg font-md pt-1">from $356</label>
         </button>
